7152b43c68aa1c7bad60de7a1fc744d1983061e5
raincoat0503 <raincoat.503@gmail.com>
2022-01-31 16:03:02 +0900

    ファーストコミット php,scssのファイル設計 ヘッダーのアイコングループの修正

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accessory</title>
+    <?php include('./php/module/font.php'); ?>
+    <link rel="stylesheet" href="./scss/style.min.css">
+</head>
+
+<body>
+    <?php include('./php/module/header.php'); ?>
+    <?php include('./php/module/footer.php'); ?>
+
+    <?php include('./php/script.php'); ?>
+</body>
+
+</html>
---
+++ php/module/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+    <div class="header__logo" ><img src="./media/img/module/logo.svg" alt=""></div>
+    <ul class="header__nav navlist">
+        <li class="navlist__item"><a href="#">HOME</a></li>
+        <li class="navlist__item"><a href="#">MEN'S</a></li>
+        <li class="navlist__item"><a href="#">WOMEN'S</a></li>
+        <li class="navlist__item"><a href="#">ABOUT</a></li>
+    </ul>
+    <a class="header__icon" href="#"><img src="./media/img/icon/user.svg" alt="user"></a>
+    <a class="header__icon" href="#"><img src="./media/img/icon/bag.svg" alt="bag"></a>
+</div>
